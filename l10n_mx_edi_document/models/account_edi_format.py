db3657500cb929b5d20024017cd3dcfb6d062f4c
lquintero019 <lquintero019@gmail.com>
2022-02-22 11:54:50 -0600

    [UPD] modulo l10n_mx_edi_document restablecer a borrador

--- l10n_mx_edi_document/models/account_edi_format.py
+++ l10n_mx_edi_document/models/account_edi_format.py
@@ -2,10 +2,10 @@ from odoo import models
 
 
 class AccountEdiFormat(models.Model):
-    _inherit = "account.edi.format"
+    _inherit = 'account.edi.format'
 
     def _is_required_for_invoice(self, invoice):
         """Avoid generate a new CFDI"""
-        # if invoice.edi_state == 'sent':
-        #     return False
+        if invoice.edi_state == 'sent':
+            return False
         return super(AccountEdiFormat, self)._is_required_for_invoice(invoice)

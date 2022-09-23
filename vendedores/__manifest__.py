e564308ba01b1eb7be0c8e49bafcaf4614002c68
JuanEmus <emus75@hotmail.com>
2022-09-22 21:43:23 -0600

    [ADD]Se añaden 2 modulos, uno llamado vistaproveedores donde se modifica la vista de lista de los proveedores añadiendo a las columnas el campo REF de los proveedores, el otro es el modulo de vendedores, donde se realiza un grupo para añadir seguridad a la visualizacion de los vendedores, en estecaso, se ocultan los campos de costo, margen y margen% de la vista de formulario en las cotizaciones de venta.

---
+++ vendedores/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Permisos vendedores',
+    'version': '13.0.0.1.0',
+    'author': 'JuanEmus',
+    'depends': [
+        'sale_management',
+    ],
+    'data': [
+        # security
+        'security/security.xml',
+        # 'security/ir.model.access.csv',
+        # data
+        # demo
+        # reports
+        # views
+        'views/sale_order_hide.xml',
+        'views/sale_order_line_hide.xml',
+    ],
+}
---
+++ vistaproveedores/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Vista proveedores',
+    'version': '13.0.0.1.2',
+    'author': 'JuanEmus',
+    'depends': [
+        'purchase',
+    ],
+    'data': [
+        # security
+        # data
+        # demo
+        # reports
+        # views
+        'views/partner_tree_view_ref.xml',
+    ],
+}
